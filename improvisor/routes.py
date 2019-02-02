d41c6186235526824ba5c6a585421d7c53070b11
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2019-02-02 22:01:45 +0000

    Added controller and presenter pages

--- improvisor/routes.py
+++ improvisor/routes.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, url_for, jsonify, request, redirect, session, flash
 from flask_bootstrap import Bootstrap
-import os, json, string, datetime, bcrypt, urllib, hashlib
+import os, json, string, datetime, urllib, hashlib
 from improvisor import app
 
 @app.route('/', methods=['GET'])
 def index():
 	tags = ['database', 'server', 'improvise', 'group project', 'image', 'youtube video']
 	return render_template('index.html', tags=tags)
+
+@app.route('/presenter', methods=['GET'])
+def presenter():
+	return render_template('presenter.html')
+
+@app.route('/controller', methods=['GET'])
+def controller():
+	tags = ['database', 'server', 'improvise', 'group project', 'image', 'youtube video']
+	return render_template('controller.html', tags=tags)
